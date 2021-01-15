7b05cb84252feaca9df99845b60ffdfa57a8c907
ZC <1039110967@qq.com>
2021-01-15 10:34:15 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2021-01-15-上午 10:16
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
